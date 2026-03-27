d7722058a649e1c8150337ec852e576f29270ad0
Hasna <hasn.outaleb@edu.umi.ac.ma>
2026-03-27 13:05:43 +0100

    premier commit

---
+++ tests/test_tasks.py
@@ -0,0 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../app")))
+def test_add_task():
+  add_task("Learn Docker")
+  assert "Learn Docker"in get_tasks()
+def test_multiple_tasks():
+  add_task("Learn Ci")
+  add_task("Learn Devops")
+  tasks=get_tasks()
+  assert "Learn Ci"in tasks
+  assert "Learn Devops"in tasks
